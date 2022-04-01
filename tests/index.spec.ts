1c59a84291cef8146450bcf2890956ba3a8fe2ec
Olyno <olyno.dev@gmail.com>
2022-04-01 22:43:45 +0200

    style: format

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('Navigate to index page and ensure h1 test', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
