390a623b4f9f5186811bce3275dce5c4a48159eb
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-02 22:17:09 +0100

    Initial Auth Logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from "next/app";
+import { UserProvider } from '@supabase/supabase-auth-helpers/react';
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs';
 import "windi.css";
+import "../styles/reset.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp;
