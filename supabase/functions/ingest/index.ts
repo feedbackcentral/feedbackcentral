4e3dc090a753795f7a29b6ad1f9b5e7786c485cd
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-02 22:25:56 +0100

    Add cron demo

--- supabase/functions/ingest/index.ts
+++ supabase/functions/ingest/index.ts
@@ -1,6 +1,10 @@
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import {daily} from 'https://deno.land/x/deno_cron/cron.ts';
 
 // TODO add cron jobs here!
+daily(() => {
+  console.log("Cron demo from Supabase edge functions!")
+});
 
 serve(async (req) => {
   return new Response(
