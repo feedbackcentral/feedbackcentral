1807a09f7e4f01456cbe7d300b3124321b978746
Harry Bairstow <h.bairstow22@gmail.com>
2022-04-02 22:43:28 +0100

    Add raw .sql files

--- sql/mindsdb.sql
+++ sql/mindsdb.sql
@@ -1,1 +1,14 @@
--- TODO Put MindsDB Code here
+CREATE DATABASE supabase
+WITH ENGINE = "postgres",
+PARAMETERS = {
+    "user": "user",
+    "password": "password",
+    "host": "localhost",
+    "port": "5432",
+    "database": "postgres"
+};
+
+CREATE PREDICTOR mindsdb.feedback_predictor
+FROM supabase
+  (SELECT content, classification FROM feedback)
+PREDICT classification;
