61bfaaec10057b870d851af6954d337f0dbc1359
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-03 17:54:40 +0100

    Create login.tsx
    This is tempory for testing and should be replaced with a real version eventually

---
+++ pages/login.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from "next";
+import { useUser, Auth } from '@supabase/supabase-auth-helpers/react';
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs';
+
+const LoginPage: NextPage = () => {
+    const { user, error } = useUser();
+
+    return (
+        <div className="w-full h-full flex justify-center items-center bg-gray-50">
+            <div className="w-1/2">
+                {error && <p>{error.message}</p>}
+                {/* TODO Replace with a custom made UI */}
+                <Auth
+                    supabaseClient={supabaseClient}
+                    providers={['twitter']}
+                    socialColors={true}
+                    magicLink={true}
+                    socialLayout="horizontal"
+                    socialButtonSize="xlarge"
+                    className="p-4 bg-white rounded-xl shadow-light-500"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
