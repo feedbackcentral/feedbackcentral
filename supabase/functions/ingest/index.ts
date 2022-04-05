ce76babdafc976d42677691b71b6c4d2d64ad6bc
VictorPeralta <peralta.r.victor@gmail.com>
2022-04-04 23:49:31 -0700

    docstring

--- supabase/functions/ingest/index.ts
+++ supabase/functions/ingest/index.ts
@@ -5,6 +5,15 @@ const client = new TwitterClient(
   "AAAAAAAAAAAAAAAAAAAAACDzawEAAAAA2BcTEqB5qI%2Ftbbx7%2BxHXg0Qm5qU%3DNwZ0O6SMgT3pSsw0nygaPtwwdiz6Lzkxpsg8I6LTYviMzrJwOJ"
 );
 
+/**
+ * Takes information about a source that is ready to be ingested from, then
+ * 1. Ingests feedback accordingly to the source type.
+ *  a. If the source is a Twitter account, it will ingest all of the tweets where this account is mentioned.
+ *  b. If the source is a Twitter hashtag, it will ingest all of the tweets where this hashtag is mentioned.
+ *  c. If the source is a Twitter search, it will ingest all of the tweets that match the search query.
+ * 2. Saves feedback to the database.
+ * 3. Updates the source's last_run to now, and next_ingest_at field to the next time the source should be ingested.
+ */
 serve(async (req: Request) => {
   const body = (await req.json()) as RequestBody;
 
@@ -17,6 +26,8 @@ serve(async (req: Request) => {
 
     if (userId) {
       const mentions = await client.getMentionsByUserId(userId, tweetParams);
+
+      //Now we can store mentions in the database. We should also do classification here.
     }
   }
 
